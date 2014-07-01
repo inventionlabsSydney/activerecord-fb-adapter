d9e585d3814be9c2e589765df74669a215df9372
Karl Kloppenborg <karl@inventionlabs.com.au>
2014-07-02 04:51:39 +1000

    Changing Arel Update Node to remove quotations on tables

--- lib/active_record/connection_adapters/fb_adapter.rb
+++ lib/active_record/connection_adapters/fb_adapter.rb
@@ -24,7 +24,7 @@ module Arel
       def visit_Arel_Nodes_UpdateStatement o, *a
         [
           "UPDATE #{visit(o.relation).gsub(/"/, '')}",
-          ("SET #{o.values.map { |value| visit(value) }.join ', '}" unless o.values.empty?),
+          ("SET #{o.values.map { |value| visit(value).gsub(/"/, '') }.join ', '}" unless o.values.empty?),
           ("WHERE #{o.wheres.map { |x| visit(x) }.join ' AND '}" unless o.wheres.empty?),
           (visit(o.limit) if o.limit),
         ].compact.join ' '
