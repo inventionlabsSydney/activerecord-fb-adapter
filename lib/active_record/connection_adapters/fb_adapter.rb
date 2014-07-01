6cf9a25cdb9d7f4efda0a7334aa32f553f662ee7
Karl Kloppenborg <karl@inventionlabs.com.au>
2014-07-02 03:43:27 +1000

    Adding Arel Override of Insert to allow none-table-quotes for the old dialects of FireBird

--- lib/active_record/connection_adapters/fb_adapter.rb
+++ lib/active_record/connection_adapters/fb_adapter.rb
@@ -38,6 +38,7 @@ module Arel
         "SKIP #{visit(o.expr)}"
       end
       def visit_Arel_Nodes_InsertStatement o, *a
+        print o.inspect
         [
           "INSERT INTO  #{visit(o.relation).gsub(/"/, '')}",
           "(#{o.columns.map { |x| x.name }.join ', '})",
