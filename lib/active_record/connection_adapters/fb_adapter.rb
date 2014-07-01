c4a27d838384d80570ba28004e27880acfe0d326
Karl Kloppenborg <karl@inventionlabs.com.au>
2014-07-02 03:01:00 +1000

    Adding Arel Override test

--- lib/active_record/connection_adapters/fb_adapter.rb
+++ lib/active_record/connection_adapters/fb_adapter.rb
@@ -38,7 +38,7 @@ module Arel
         "SKIP #{visit(o.expr)}"
       end
       def visit_Arel_Nodes_InsertStatement o, *a
-        "INSERT INTO #{visit.gsub(/"/, '') o.relation}"
+        "INSERT INTO #{visit(o.relation).gsub(/"/, '')}"
       end
 
 
