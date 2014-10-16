d56b823693ca707640a4cf2f3b6b1796109c1c2e
Karl Kloppenborg <karl@inventionlabs.com.au>
2014-10-16 18:23:54 +1100

    Updated sequence counter

--- lib/active_record/connection_adapters/fb_adapter.rb
+++ lib/active_record/connection_adapters/fb_adapter.rb
@@ -730,8 +730,9 @@ module ActiveRecord
       end
 
       def next_sequence_value(sequence_name)
-        select_one("SELECT NEXT VALUE FOR #{sequence_name} FROM RDB$DATABASE").values.first unless legacy_mode
-        select_one("SELECT GEN_ID(#{sequence_name}, 1) FROM RDB$DATABASE").values.first if legacy_mode
+        #select_one("SELECT NEXT VALUE FOR #{sequence_name} FROM RDB$DATABASE").values.first unless legacy_mode
+        select_one("SELECT GEN_ID(#{sequence_name}, 1) FROM RDB$DATABASE").values.first
+
       end
 
       # Inserts the given fixture into the table. Overridden in adapters that require
